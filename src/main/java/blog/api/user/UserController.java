38662ed68f43a0fef2e881f37707dffa6d603a9f
baiching <bachmarma@gmail.com>
2025-04-12 08:13:40 +0200

    name conflict so changed to UserController

--- src/main/java/blog/api/user/UserController.java
+++ src/main/java/blog/api/user/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/profile")
-public class ProfileController {
+public class UserController {
     @Autowired
     private ProfileService profileService;
 
